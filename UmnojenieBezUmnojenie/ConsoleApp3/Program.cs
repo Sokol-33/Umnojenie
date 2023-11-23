33382078120bbc2f670f5f4f94917065cf113202
Сокол <118041104+Sokol-33@users.noreply.github.com>
2023-11-23 09:31:23 +0300

    Add files via upload

---
+++ UmnojenieBezUmnojenie/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Второе число");
+int b = int.Parse(Console.ReadLine());
+int result = 0;
+int number1 = Math.Sign(a);
+int number2 = Math.Sign(b);
+
+a = Math.Abs(a);
+b = Math.Abs(b);
+for (var i = 0; i < a; i++) result += b;
+if (number1 == number2)
+{
+    Console.WriteLine("Ответ:" +result) ;
+}
+else
+{
+    Console.WriteLine("Ответ:" +(- result));
+}
